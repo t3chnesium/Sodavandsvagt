94d0fc586ffd24822e4860d68234b165b4b0d9e8
t3chnesium <76813083+t3chnesium@users.noreply.github.com>
2026-03-03 20:08:14 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE='sodavand-sb-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.url.includes('supabase.co')) return;
+  e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).catch(()=>caches.match('./index.html'))));
+});
